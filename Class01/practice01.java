dc0e445432e9b4aad3300beec2bf68bce1e78310
hongseop <a01082372487@gmail.com>
2023-08-22 13:40:03 +0900

    UPDATE::GUI를 사용해서 두 문제의 답의 합을 최종적으로 알려주는 코드

---
+++ Class01/practice01.java
@@ -0,0 +1,27 @@
+package Class01;
+import javax.swing.JOptionPane;
+
+public class practice01 {
+    public static void main(String[] args){
+
+        int n1, n2;
+        int sumOfCorrectAnswers=0;
+
+        for(int i = 0; i < 2; i++) {
+            n1 = (int) (Math.random() * 8) + 2; // 2 ~ 9;
+            n2 = (int) (Math.random() * 9) + 1; // 1 ~ 9;
+
+            String sAnswer = JOptionPane.showInputDialog(n1 + "X" + n2 + "=?");
+            int ans = Integer.parseInt(sAnswer);
+
+            if (ans == n1 * n2) {
+                JOptionPane.showMessageDialog(null, "정답!");
+                sumOfCorrectAnswers += ans;
+            } else {
+                JOptionPane.showMessageDialog(null, "틀렸음 ㅋ");
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "정답들의 합은 " + sumOfCorrectAnswers + "입니다!");
+    }
+}
