ea773d49e712bebd618b1d60f12ba7585a7607f0
hongseop <a01082372487@gmail.com>
2023-08-28 17:19:48 +0900

    UPDATE :: JAVA방과후 두번째 수업 주석추가

--- Class02/operator.java
+++ Class02/operator.java
@@ -7,10 +7,11 @@ public class operator {
         System.out.println(1<1);
         System.out.println(1>=1);
 
-        //2와 7을 출력해서
+        //2와 7을 출력해서 false 출력
         System.out.println(2>7);
         System.out.println(2==7);
 
+        //2, 5, 10을 이용해서 true 출력
         System.out.println(2+5<10);
         System.out.println(2*5==10);
         System.out.println((10!=2)||(5==2));
