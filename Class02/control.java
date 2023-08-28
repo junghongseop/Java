c1659683cf149d0541663f404176a9764b3c8517
hongseop <a01082372487@gmail.com>
2023-08-28 19:11:57 +0900

    UPDATE :: JAVA_SCHOOL 제어문

---
+++ Class02/control.java
@@ -0,0 +1,21 @@
+package Class02;
+
+public class control {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println("=== while ===");
+
+        int i = 0;
+        while(i<3) {
+            System.out.println(2);
+            System.out.println(3);
+            i++;
+        }
+        System.out.println("=== for ===");
+        for(i = 0; i<3; i++) {
+            System.out.println(2);
+            System.out.println(3);
+        }
+        System.out.println(4);
+    }
+}
