935fccfcbf9d31144f2b9dcb7e444cbf18da2740
hongseop <a01082372487@gmail.com>
2023-08-28 17:18:32 +0900

    UPDATE :: JAVA방과후 두번째 수업 (연산자)추가

---
+++ Class02/operator.java
@@ -0,0 +1,18 @@
+package Class02;
+
+public class operator {
+    public static void main(String[] args) {
+        System.out.println(1>1);    //false
+        System.out.println(1==1);   //true
+        System.out.println(1<1);
+        System.out.println(1>=1);
+
+        //2와 7을 출력해서
+        System.out.println(2>7);
+        System.out.println(2==7);
+
+        System.out.println(2+5<10);
+        System.out.println(2*5==10);
+        System.out.println((10!=2)||(5==2));
+    }
+}
