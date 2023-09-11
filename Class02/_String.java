117a2bcf039716e475a89de7f45b88a3cdb88134
hongseop <a01082372487@gmail.com>
2023-09-11 16:51:53 +0900

    UPDATE :: 문자열 연습 코드(8월 28일 수업)

---
+++ Class02/_String.java
@@ -0,0 +1,11 @@
+package Class02;
+
+public class _String {
+    public static void main(String[] args) {
+        String str1 = new String("안녕");
+        String  str2 = str1;
+        str1 = "안녕하세요";
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
