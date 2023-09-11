9833d50b3c7257fea1c2beba4f64a4224def0f96
hongseop <a01082372487@gmail.com>
2023-09-11 17:15:32 +0900

    FIX :: 조금 더 간결하게 코드를 수정함

--- Class03/ex01.java
+++ Class03/ex01.java
@@ -7,19 +7,16 @@ public class ex01 {
         Scanner s1 = new Scanner(System.in);
         String s = s1.next();
         int[] ch = new int [26];
+        char c;
 
         //반복문을 이용해 문자열 s의 알파벳 빈도를 계산
         for (int i = 0; i < s.length(); i++) {
-            char c = Character.toLowerCase(s.charAt(i));
-            if (c >= 'a' && c <= 'z') {
-                ch[c - 'a']++;
-            }
+            ch[s.charAt(i) - 'a']++;
         }
         //반복문을 이용해 빈도가 0이 아닌 알파벳과 개수를 출력
         for (int i = 0; i < 26; i++) {
             if (ch[i] != 0) {
-                char alphabet = (char) ('a' + i);
-                System.out.println(alphabet + " : " + ch[i]);
+                System.out.printf("%c : %d\n", 'a' + i, ch[i]);
             }
         }
     }
