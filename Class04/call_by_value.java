2287a745f8ee3dc6745a5062207b27e13a9d65b3
hongseop <a01082372487@gmail.com>
2023-09-18 17:01:24 +0900

    ADD :: java에서 call by value를 설명하기 위해 쓴 code

--- Class04/call_by_value.java
+++ Class04/call_by_value.java
@@ -2,11 +2,11 @@ package Class04;
 
 public class call_by_value {
     static void increase( int a ){
-        a++;
+        a++;    //사용되지 않는 코드
     }
     public static void main(String[] args) throws Exception {
         int n = 10;
         increase(n);
-        System.out.println(n);
+        System.out.println(n);  //10이 출력
     }
 }
