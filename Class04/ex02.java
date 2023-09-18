15dc7a17cadf04a5acd8f2e2656aa88956ef3f8c
hongseop <a01082372487@gmail.com>
2023-09-18 16:47:20 +0900

    ADD :: 메서드 입력값

---
+++ Class04/ex02.java
@@ -0,0 +1,16 @@
+package Class04;
+
+public class ex02 {
+
+    public static void numbering(int init, int limit){
+        int i = init;
+        while (i < limit) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        numbering(1, 5);
+    }
+}
