e13e84ed82d62c469a37d1ec281caa49251e747f
hongseop <a01082372487@gmail.com>
2023-09-18 16:53:28 +0900

    test

--- Class04/ex02.java
+++ Class04/ex02.java
@@ -2,7 +2,7 @@ package Class04;
 
 public class ex02 {
 
-    public static void numbering(int init, int limit){
+    public static void numbering(int init, int limit) {
         int i = init;
         while (i < limit) {
             System.out.println(i);
@@ -11,6 +11,6 @@ public class ex02 {
     }
 
     public static void main(String[] args) {
-        numbering(1, 5);
+        numbering(1, 5);  //numbering 메소드로 1과 5라는 인자를 전달한다.
     }
 }
