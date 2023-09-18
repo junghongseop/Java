c0b45ebc64123e8f3624e46def751879f0f4131e
hongseop <a01082372487@gmail.com>
2023-09-18 16:42:17 +0900

    ADD :: 메서드 호출 및 정의(9월 18일 자바 방과후)

---
+++ Class04/ex01.java
@@ -0,0 +1,16 @@
+package Class04;
+class ex01 {
+
+    public static void numbering() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        //numbering 메소드를 실행
+        numbering();
+    }
+}
